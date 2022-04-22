69927e928f8d25715df9c1ab235c6cb0d49e3f91
Zachary Fetters <zachfett@protonmail.com>
2022-04-21 21:59:36 -0400

    Initial commit

---
+++ xTAE/src/xTAE.cpp
@@ -0,0 +1,12 @@
+﻿// xTAE.cpp - Engine entry point. Build and launch this to launch your game.
+// Copyright (c) Zachary Fetters, 2022. Licensed and available under the MIT license.
+
+#include "xTAE.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
